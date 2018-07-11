4a2e3f528ce3af3d691183d69ab9da9278a15f0a
lfolmert <lukas.folmert@freenetdigital.com>
2018-07-11 12:23:22 +0200

    simplify example

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-
-const Hello1 = import(/* webpackChunkName: "hello1" */ '@/components/HelloWorld');
+const Hello1 = import(/* webpackChunkName: "hello1" */ '@/components/HelloWorld1');
 const Hello2 = import(/* webpackChunkName: "hello2" */ '@/components/HelloWorld2');
 
 Vue.use(Router)
